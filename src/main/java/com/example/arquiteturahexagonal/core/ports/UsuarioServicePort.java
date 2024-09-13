522a32dc8427a44e1e4847c4307ac54a770e3b7c
Taís Marcolino <tais.c.marcolino@gmail.com>
2024-09-13 20:32:08 -0300

    ajuste

--- src/main/java/com/example/arquiteturahexagonal/core/ports/UsuarioServicePort.java
+++ src/main/java/com/example/arquiteturahexagonal/core/ports/UsuarioServicePort.java
@@ -1,4 +1,15 @@
 package com.example.arquiteturahexagonal.core.ports;
 
+import com.example.arquiteturahexagonal.adapters.dto.LoginDto;
+import com.example.arquiteturahexagonal.core.domain.Usuario;
+
+import java.util.List;
+
 public interface UsuarioServicePort {
+
+    Usuario salvar(Usuario usuario);
+    Usuario buscarPorId(Long id);
+    void logar(LoginDto loginDto);
+    Usuario atualizar(Usuario usuario);
+    List<Usuario> buscarTodos();
 }
--- src/main/java/com/example/arquiteturahexagonal/core/services/UsuarioService.java
+++ src/main/java/com/example/arquiteturahexagonal/core/services/UsuarioService.java
@@ -1,6 +1,35 @@
 package com.example.arquiteturahexagonal.core.services;
 
+import com.example.arquiteturahexagonal.adapters.dto.LoginDto;
+import com.example.arquiteturahexagonal.core.domain.Usuario;
 import com.example.arquiteturahexagonal.core.ports.UsuarioServicePort;
 
+import java.util.List;
+
 public class UsuarioService implements UsuarioServicePort {
+
+    @Override
+    public Usuario salvar(Usuario usuario) {
+        return usuario;
+    }
+
+    @Override
+    public Usuario buscarPorId(Long id) {
+        return new Usuario();
+    }
+
+    @Override
+    public void logar(LoginDto loginDto) {
+
+    }
+
+    @Override
+    public Usuario atualizar(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public List<Usuario> buscarTodos() {
+        return List.of();
+    }
 }
